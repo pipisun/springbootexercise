74f9ad20f875e14240b59d0b8430e2446ea0c031
Jeremy <sst1978@163.com>
2017-06-13 15:57:38 -0500

    Initialized

---
+++ src/main/java/com/example/demo/AwardType.java
@@ -0,0 +1,7 @@
+package com.example.demo;
+
+public enum AwardType {
+	MEDAL,
+	TROPHY,
+	NONE
+}
---
+++ src/main/java/com/example/demo/TournamentMatch.java
@@ -0,0 +1,7 @@
+package com.example.demo;
+
+public class TournamentMatch extends Match {
+	private int homePoints;
+	private int visitorPoints;
+
+}
